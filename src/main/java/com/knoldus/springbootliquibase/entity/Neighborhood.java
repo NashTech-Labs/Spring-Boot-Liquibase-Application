b35f77dd6cdfa348fd67d2e63cda537dfe9c003b
Shashikant-15 <shashikant.tanti@knoldus.com>
2022-10-13 00:26:35 +0530

    first commit

---
+++ src/main/java/com/knoldus/springbootliquibase/entity/Neighborhood.java
@@ -0,0 +1,23 @@
+package com.knoldus.springbootliquibase.entity;
+
+import com.knoldus.springbootliquibase.entity.base.BaseEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class Neighborhood extends BaseEntity {
+
+    @Column(nullable = false, length = 80)
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 7)
+    private String code;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "district_id", nullable = false)
+    private District district;
+}
---
+++ src/main/java/com/knoldus/springbootliquibase/entity/base/BaseEntity.java
@@ -0,0 +1,22 @@
+package com.knoldus.springbootliquibase.entity.base;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.util.UUID;
+
+@Data
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column
+    private boolean deleted = Boolean.FALSE;
+
+}
